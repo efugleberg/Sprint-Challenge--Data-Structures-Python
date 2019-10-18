15925cebaa6072599300f235fbfb7ef1adfcede5
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-10-18 10:55:44 -0500

    names complete

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -11,12 +12,26 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+bst = BinarySearchTree(names_1[0])
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 != names_1[0]:
+        bst.insert(name_1)
+
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
+    if name_1 == name_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# Original Run Time is 6.43066 seconds and is O(n**2)
+# My bst run time is 0.12595 seconds and is O(n log(n))
 
